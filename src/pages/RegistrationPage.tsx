b3d24f294c77df77ea38046cce8888368546bbbf
Bakirovildar <bakirovildar2011@mail.ru>
2024-04-29 19:28:19 +0500

    first commit

---
+++ src/pages/RegistrationPage.tsx
@@ -0,0 +1,9 @@
+import './registrationPage.css'
+
+export const RegistrationPage = () => {
+    return (
+        <div className="registration-container">
+            <h1>Регистрация</h1>
+        </div>
+    )
+}
