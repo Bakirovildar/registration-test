bf284e5855d1863e21efa12ac5bccfb6fc994719
Bakirovildar <bakirovildar2011@mail.ru>
2024-05-02 17:44:47 +0500

    годы

--- src/components/inputs/calendar/CustomCalendar.tsx
+++ src/components/inputs/calendar/CustomCalendar.tsx
@@ -1,8 +1,8 @@
-import { Calendar } from 'primereact/calendar';
+import {Calendar} from 'primereact/calendar';
 import iconCalendar from '../../../assests/svg/IconCalendar.svg'
 import './customCalendar.css'
-import { useEffect, useState } from 'react';
-import { Nullable } from 'primereact/ts-helpers';
+import {useEffect, useState} from 'react';
+import {Nullable} from 'primereact/ts-helpers';
 import {addLocale} from "primereact/api";
 
 interface ICustomCalendar {
@@ -12,14 +12,15 @@ interface ICustomCalendar {
     isInvalid?: boolean
     onChange: (date: Nullable<Date>) => void
 }
+
 addLocale('ru', {
     firstDayOfWeek: 1,
     dayNames: ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
-    dayNamesMin:['ВС', 'ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ'],
+    dayNamesMin: ['ВС', 'ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ'],
     monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
     monthNamesShort: ["Янв", "Фев", "Мар", "Апр", "Май", "Июн", "Июл", "Авг", "Сен", "Окт", "Ноя", "Дек"]
 });
-export const CustomCalendar = ({ dateFormat, required, headTitle, isInvalid, onChange }: ICustomCalendar) => {
+export const CustomCalendar = ({dateFormat, required, headTitle, isInvalid, onChange}: ICustomCalendar) => {
     const [date, setDate] = useState<Nullable<Date>>()
 
     useEffect(() => {
@@ -28,15 +29,17 @@ export const CustomCalendar = ({ dateFormat, required, headTitle, isInvalid, onC
 
     return (
         <div className='calendar'>
-            <div style={{ position: 'relative', width: '100%' }}>
-                {required ? <div className='input-required'>{headTitle}<span>*</span></div> : <div className='input-required'>{headTitle}</div>}
+            <div style={{position: 'relative', width: '100%'}}>
+                {required ? <div className='input-required'>{headTitle}<span>*</span></div> :
+                    <div className='input-required'>{headTitle}</div>}
                 <Calendar className={`p-calendar-endicon ${isInvalid && 'error-calendar'}`}
                           dateFormat={dateFormat ? dateFormat : "dd.mm.yy"}
                           value={date}
                           onChange={(e) => setDate(e.value)}
+                          view={dateFormat === "yy" ? "year" : "date"}
                           locale='ru'/>
                 <span className="calendar-icon">
-                    <img src={iconCalendar} alt="" />
+                    <img src={iconCalendar} alt=""/>
                 </span>
                 {isInvalid && <div className='error-message'>Обязательное поле</div>}
             </div>
